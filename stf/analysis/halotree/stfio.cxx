6444e27b794ebdf87ab54bfd3bb23e421b15a000
pelahi <pascaljelahi@gmail.com>
2016-08-04 09:59:26 +0800

    correction to reading hdf format velociraptor output in tree code

--- stf/analysis/halotree/stfio.cxx
+++ stf/analysis/halotree/stfio.cxx
@@ -616,6 +616,14 @@ Int_t MPIReadHaloGroupCatalogDataParticleNum(char* infile, int mpi_ninput, int i
                 Fgroup.read((char*)numingroup,sizeof(Int_t)*nglocal);
                 for (Int_t i=0;i<nglocal;i++) TotalNumberofParticles+=numingroup[i];
             }
+            if (ibinary==INHDF) {
+                dataset=Fhdfgroup.openDataSet(hdfnames.group[3]);
+                dataspace=dataset.getSpace();
+                data=::operator new(sizeof(unsigned int)*(nglocal+1))
+                dataset.read(data,hdfnames.groupdatatype[3],dataspace);
+                for (Int_t i=0;i<nglocal;i++) TotalNumberofParticles+=((unsigned int*)data)[i];
+                ::operator delete(data);
+            }
             else {
                 for (Int_t i=0;i<nglocal;i++) {Fgroup>>numingroup[i];TotalNumberofParticles+=numingroup[i];}
             }
@@ -629,6 +637,14 @@ Int_t MPIReadHaloGroupCatalogDataParticleNum(char* infile, int mpi_ninput, int i
                 if (ibinary==INBINARY) {
                     Fsgroup.read((char*)numingroup,sizeof(Int_t)*nsglocal);
                     for (Int_t i=0;i<nsglocal;i++) TotalNumberofParticles+=numingroup[i];
+                }
+                if (ibinary==INHDF) {
+                    dataset=Fhdfsgroup.openDataSet(hdfnames.group[3]);
+                    dataspace=dataset.getSpace();
+                    data=::operator new(sizeof(unsigned int)*(nglocal+1))
+                    dataset.read(data,hdfnames.groupdatatype[3],dataspace);
+                    for (Int_t i=0;i<nglocal;i++) TotalNumberofParticles+=((unsigned int*)data)[i];
+                    ::operator delete(data);
                 }
                 else {
                     for (Int_t i=0;i<nsglocal;i++) {Fsgroup>>numingroup[i];TotalNumberofParticles+=numingroup[i];}
