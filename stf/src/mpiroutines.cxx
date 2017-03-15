298fc1b4a65d1a66d5b54adb7de0a3dbfba88c31
pelahi <pascaljelahi@gmail.com>
2017-03-15 16:27:21 +0800

    minor update to distributed parallel read

--- stf/src/mpiroutines.cxx
+++ stf/src/mpiroutines.cxx
@@ -357,8 +357,9 @@ int MPISearchForOverlap(Particle &Part, Double_t &rdist){
 void MPIDistributeReadTasks(Options&opt, int *&ireadtask, int*&readtaskID){
     //initialize
     if (opt.num_files<opt.nsnapread) opt.nsnapread=opt.num_files;
+    if (opt.nsnapread<NProcs) opt.nsnapread=NProcs;
     for (int i=0;i<NProcs;i++) ireadtask[i]=-1;
-    int spacing=floor(NProcs/opt.nsnapread);
+    int spacing=max(1,floor(NProcs/opt.nsnapread));
     for (int i=0;i<opt.nsnapread;i++) {ireadtask[i*spacing]=i;readtaskID[i]=i*spacing;}
 }
 //@}
