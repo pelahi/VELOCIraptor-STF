10684ee26f322c7f44b98895a3a7c10e984d5008
pelahi <pascaljelahi@gmail.com>
2017-07-24 15:33:10 +0800

    more output debugging

--- stf/analysis/treefrog/io.cxx
+++ stf/analysis/treefrog/io.cxx
@@ -467,7 +467,7 @@ void WriteHaloMergerTree(Options &opt, DescendantData **p, HaloTreeData *h) {
                         Fout<<h[i].Halo[j].haloID<<"\t"<<p[i][j].NumberofDescendants<<endl;
                         for (int k=0;k<p[i][j].NumberofDescendants;k++) {
                             //Fout<<h[i+p[i][j].istep].Halo[p[i][j].DescendantList[k]-1].haloID<<endl;
-                            Fout<<p[i][j].DescendantList[k]<<" "<<p[i][j].istep<<" "<<h[i+p[i][j].istep].Halo[p[i][j].DescendantList[k]-1].haloID<<endl;
+                            Fout<<p[i][j].DescendantList[k]<<" "<<p[i][j].istep<<" "<<h[i+p[i][j].istep].Halo[p[i][j].DescendantList[k]-1].haloID<<" "<<p[i][j].Merit[k]<<endl;
                         }
                     }
                 }
@@ -511,7 +511,8 @@ void WriteHaloMergerTree(Options &opt, DescendantData **p, HaloTreeData *h) {
                 for (int j=0;j<h[i].numhalos;j++) {
                     Fout<<h[i].Halo[j].haloID<<"\t"<<p[i][j].NumberofDescendants<<endl;
                     for (int k=0;k<p[i][j].NumberofDescendants;k++) {
-                        Fout<<h[i+p[i][j].istep].Halo[p[i][j].DescendantList[k]-1].haloID<<endl;
+                        Fout<<p[i][j].DescendantList[k]<<" "<<p[i][j].istep<<" "<<h[i+p[i][j].istep].Halo[p[i][j].DescendantList[k]-1].haloID<<" "<<p[i][j].Merit[k]<<endl;
+//                        Fout<<h[i+p[i][j].istep].Halo[p[i][j].DescendantList[k]-1].haloID<<endl;
                     }
                 }
             }
