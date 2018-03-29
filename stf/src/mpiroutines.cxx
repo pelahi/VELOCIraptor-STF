95cc5f2ea9ccd06819ad089d04558c06f210e876
Pascal Jahan Elahi <pascaljelahi@gmail.com>
2018-03-29 18:06:10 +0800

    bug fix to mesh mpi search

--- stf/src/mpiroutines.cxx
+++ stf/src/mpiroutines.cxx
@@ -3248,13 +3248,13 @@ vector<int> MPIGetCellListInSearchUsingMesh(Options &opt, Double_t xsearch[3][2]
         for (auto iy=iystart;iy<=iyend;iy++){
             for (auto iz=izstart;iz<=izend;iz++){
                 index=0;
-                if (iz<0) index+=opt.numcellsperdim-iz;
+                if (iz<0) index+=opt.numcellsperdim+iz;
                 else if (iz>=opt.numcellsperdim) index+=iz-opt.numcellsperdim;
                 else index+=iz;
-                if (iy<0) index+=(opt.numcellsperdim-iy)*opt.numcellsperdim;
+                if (iy<0) index+=(opt.numcellsperdim+iy)*opt.numcellsperdim;
                 else if (iy>=opt.numcellsperdim) index+=(iy-opt.numcellsperdim)*opt.numcellsperdim;
                 else index+=iy*opt.numcellsperdim;
-                if (ix<0) index+=(opt.numcellsperdim-ix)*opt.numcellsperdim*opt.numcellsperdim;
+                if (ix<0) index+=(opt.numcellsperdim+ix)*opt.numcellsperdim*opt.numcellsperdim;
                 else if (ix>=opt.numcellsperdim) index+=(ix-opt.numcellsperdim)*opt.numcellsperdim*opt.numcellsperdim;
                 else index+=ix*opt.numcellsperdim*opt.numcellsperdim;
                 cout<<ix<<" "<<iy<<" "<<iz<<" "<<index<<endl;
