6bb9feda0cf852346eec7b47e705d40139bf067c
Rodrigo Tobar <rtobar@icrar.org>
2019-05-10 14:11:29 +0800

    Basic labels for M200/R200 plot
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- .travis/plots/m200_r200.py
+++ .travis/plots/m200_r200.py
@@ -7,6 +7,10 @@ input_fname = sys.argv[1]
 output_fname = sys.argv[2]
 with h5py.File(input_fname) as f:
     m200, r200 = f['/Mass_200mean'].value, f['/R_200mean'].value
-pyplot.xscale('log'); pyplot.yscale('log')
+
 pyplot.plot(r200, m200, marker='.', linestyle=' ')
+pyplot.xlabel('R200_mean')
+pyplot.xscale('log')
+pyplot.ylabel('M200_mean')
+pyplot.yscale('log')
 pyplot.savefig(output_fname)
