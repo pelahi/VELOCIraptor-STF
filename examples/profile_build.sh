c5d3bf6fd52b7ea858b911181eb0c2d893227acb
pelahi <pascaljelahi@gmail.com>
2019-08-28 13:55:50 +0800

    bug fix in script.

--- examples/profile_build.sh
+++ examples/profile_build.sh
@@ -30,7 +30,7 @@ workingdir=`pwd`
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 #build exe
-cd ${scriptdir}../
+cd ${scriptdir}/../
 mkdir build-${buildlabel}
 cd build-${buildlabel}
 rm -rf *
