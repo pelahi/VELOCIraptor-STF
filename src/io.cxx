12e3ef3f1c9dc74504546df8be166f4a9385a82f
Pascal <pascaljelahi@gmail.com>
2020-04-20 14:13:58 +0800

    bug fix for parallel hdf5 writing unit information

--- src/io.cxx
+++ src/io.cxx
@@ -2680,7 +2680,7 @@ void WriteProperties(Options &opt, const Int_t ngroups, PropData *pdata){
 #ifdef USEPARALLELHDF
     if (ThisWriteTask==0) {
 #endif
-    Fhdf.append(string(fname));
+    Fhdf.append(string(fname), H5F_ACC_RDWR, ThisWriteTask, false);
     for (auto ientry=0;ientry<head.headerdatainfo.size();ientry++) {
         WriteHeaderUnitEntry(opt, Fhdf, head.headerdatainfo[ientry], head.unitdatainfo[ientry]);
     }
