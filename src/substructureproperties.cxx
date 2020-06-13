cbcc3379dd831581cb2b212f7e9b7cfcde70090f
Pascal <pascaljelahi@gmail.com>
2020-06-13 09:23:25 +0800

    Update to limits in root finding

--- src/substructureproperties.cxx
+++ src/substructureproperties.cxx
@@ -3945,7 +3945,7 @@ double CalcConcentrationRootFindingRhalf(double rratio, double tol)
     gsl_root_fsolver *s;
     double cval = 2.3;
     //start point for concentration
-    double x_lo = 0.1, x_hi = 10000.0;
+    double x_lo = 0.1, x_hi = 5000.0;
     gsl_function F;
     F.function = &mycNFWRhalf;
     F.params = &rratio;
@@ -3975,7 +3975,7 @@ double CalcConcentrationRootFindingVmax(double VmaxVvir2, double tol)
     gsl_root_fsolver *s;
     double cval = 2.3;
     //start point for concentration
-    double x_lo = 1.9, x_hi = 1000.0;
+    double x_lo = 1.9, x_hi = 5000.0;
     gsl_function F;
     F.function = &mycNFW;
     F.params = &VmaxVvir2;
