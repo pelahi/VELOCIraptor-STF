34d55cbe9becbd378d716e0ddc20f2aa09d99977
Rodrigo Tobar <rtobar@icrar.org>
2020-10-01 21:03:31 +0800

    Abort on swift-enable builds later
    When the stf executable is run, it immediately exists if the software
    was compiled to support integration with swift. This fast failure
    however prevents the executable from showing some important information
    that can be useful for debugging problems like #23. Moving the fast
    exit only a couple of lines down allows for these informational messages
    to be printed before the program exits.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- src/main.cxx
+++ src/main.cxx
@@ -20,10 +20,6 @@ using namespace velociraptor;
 
 int main(int argc,char **argv)
 {
-#ifdef SWIFTINTERFACE
-    cout<<"Built with SWIFT interface enabled when running standalone VELOCIraptor. Should only be enabled when running VELOCIraptor as a library from SWIFT. Exiting..."<<endl;
-    exit(0);
-#endif
 #ifdef USEMPI
     //start MPI
 #ifdef USEOPENMP
@@ -72,6 +68,11 @@ int main(int argc,char **argv)
 #ifdef USEOPENMP
     if (ThisTask==0) cout<<"VELOCIraptor/STF running with OpenMP. Number of openmp threads: "<<nthreads<<endl;
     if (ThisTask==0) cout<<"VELOCIraptor/STF running with OpenMP version "<< _OPENMP << endl;
+#endif
+
+#ifdef SWIFTINTERFACE
+    cout<<"Built with SWIFT interface enabled when running standalone VELOCIraptor. Should only be enabled when running VELOCIraptor as a library from SWIFT. Exiting..."<<endl;
+    exit(0);
 #endif
 
     gsl_set_error_handler_off();
