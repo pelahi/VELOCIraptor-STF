dc6d330eef60b7ca10e029d9a9af434454575daa
Claudia Lagos <claudia.lagos@icrar.org>
2021-12-21 16:10:06 +0800

    Adding most bound BH ID and position to output
    Added the position and ID of the BH that is closest to the most bound dark matter particle.
    This was tested in COLIBRE and works well.

--- src/allvars.h
+++ src/allvars.h
@@ -1621,6 +1621,10 @@ struct PropData
     ///mean accretion rate, metallicty
     Double_t acc_bh, acc_bh_mostmassive;
 
+    ///ID of most bound BH and its position
+    Int_t ibound_bh = 0;
+    Coordinate gposmbp_bh;
+
     ///\name blackhole aperture/radial profiles
     //@{
     vector<int> aperture_npart_bh;
@@ -2478,6 +2482,7 @@ struct PropData
 #endif
 #ifdef BHON
         M_bh*=opt.h;
+        gposmbp_bh*=opt.h/opt.a;
 #endif
 #ifdef HIGHRES
         M_interloper*=opt.h;
